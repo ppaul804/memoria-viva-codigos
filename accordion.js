6a99b7f41d4dc95d25aaf611db0a0b230f73b8e6
Pedro Paul <ppaul804@gmail.com>
2018-09-19 17:21:05 -0300

    Accordion em jQuery

--- accordion.js
+++ accordion.js
@@ -1,66 +1,18 @@
-var box = document.getElementsByClassName("box");
-var acc = document.getElementsByClassName("accordion");
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function (e) {
-        var thisBox = e.target.parentElement;
-        var htmlColSecBoxes = thisBox.parentElement.children;
-        // cria Array do HTMLCollection
-        var rawSecBoxes = Array.prototype.slice.call(htmlColSecBoxes);
-        var secBoxes = []; // é os boxs da seção
-        rawSecBoxes.forEach(rawSecBox => {
-            // filtra por class="box"
-            if (rawSecBox.classList.contains("box")) {
-                secBoxes.push(rawSecBox);
-            }
-        });
-
-        this.classList.toggle("active");
-
-        // defini o panel do accordion clicado
-        thisAccArray = Array.prototype.slice.call(thisBox.children);
-        var panel = thisAccArray[1];
-
-        // se tem tamanho definido, ou seja, estiver aberto
-        if (panel.style.maxHeight) {
-            console.log('FECHA');
-
-            // fecha o panel
-            panel.style.maxHeight = null;
+$(document).ready(function () {
+    $('.toggle').click(function (e) {
+        e.preventDefault();
+    
+        var $this = $(this);
+    
+        if ($this.next().hasClass('active')) {
+            $this.next().removeClass('active');
+            $this.next().slideUp(350);
         } else {
-            console.log('ABRE');
-
-            // abre o panel
-            panel.style.maxHeight = panel.scrollHeight + "px";
-
-            // atualiza o tamanho dos panels anteriores
-            var panAnt = panel.parentElement.parentElement;
-            var panAntAnt = panel.parentElement.parentElement.parentElement.parentElement;
-
-            panAnt.style.maxHeight = (panAnt.scrollHeight + panel.scrollHeight) + "px";
-            panAntAnt.style.maxHeight = (panAntAnt.scrollHeight + panAnt.scrollHeight) + "px";
-
-
-            // fecha os outros panels dos boxes da seção
-            secBoxes.forEach(secBox => {
-                // pega o panel do box
-                var secPanel = secBox.lastElementChild;
-                // se o panel da seção não for igual o panel do acc originalmente clicado
-                if (panel !== secPanel) {
-                    // considera o elemento anterior como o accordion da seção
-                    secAcc = secPanel.previousElementSibling;
-                    // se o acc não clicado tiver class = "active"
-                    if (secAcc.classList.contains("active")) {
-                        // desativa ele
-                        secAcc.classList.toggle("active");
-                    }
-                    // finalmente fecha o panel da seção
-                    secPanel.style.maxHeight = null;
-                }
-            });
+            $this.parent().parent().find('div .inner').removeClass('active');
+            $this.parent().parent().find('div .inner').slideUp(350);
+            $this.next().toggleClass('active');
+            $this.next().slideToggle(350);
         }
-        console.log("---------------------------------------------");
-        // ajuste da altura do portlet-content do liferay
-        ajusteAlturaLiferay(1);
     });
-}
+
+});
